3e64bdd09e6609aeaf0ea6d3ed0f65333ae2e376
Kumar Dara <dkumarswamy@gmail.com>
2020-11-17 22:58:52 +0530

    Parity Check

--- src/primitivetypes/ParityCheck.java
+++ src/primitivetypes/ParityCheck.java
@@ -1,40 +1,38 @@
 package primitivetypes;
 
-public class CountBits {
+public class ParityCheck {
     public static void main(String[] args) {
-        System.out.println("noOfBits:"+getCountUsingCacheTable(145892345));
-        System.out.println(2 ^3);
+        System.out.println("parity:"+getParityCacheTable(145892325));
     }
-    public static short getBitsCount(int number) {
+    public static short getParityNavie(int number) {
         short noOfBits = 0;
         while(number > 0) {
-            noOfBits += number & 1;
+            noOfBits ^= number & 1;
             number >>= 1;
         }
         return noOfBits;
     }
 
-    public static short getBitsCountBrainKerringam(int number) {
+    public static short getParityBrainKerringam(int number) {
         short noOfBits = 0;
         while(number != 0) {
-            noOfBits++;
+            noOfBits ^= 1;
             number &= (number - 1);
         }
         return noOfBits;
     }
 
-    public static short getCountUsingCacheTable(int number) {
-        short noOfBits = 0;
+    public static short getParityCacheTable(int number) {
         short[] cache = prepareLookupTable();
         //getting the last 8 bits of the number
         //reducing the number by 8 bits
-        return (short) (cache[number & 0xff] + cache[(number >> 8) & 0xff] + cache[(number >> 16) & 0xff] + cache[(number >> 24) & 0xff]);
+        return (short) (cache[number & 0xff] ^ cache[(number >> 8) & 0xff] ^ cache[(number >> 16) & 0xff] ^ cache[(number >> 24) & 0xff]);
     }
 
     public static short[] prepareLookupTable() {
         short[] cache = new short[256];
         for(short i=0; i<255; i++) {
-            cache[i] = (short) ((i & 1) + cache[i>>1]);
+            cache[i] = (short) ((i & 1) ^ cache[i>>1]);
         }
 
         return cache;
